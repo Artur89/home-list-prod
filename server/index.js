8db1bc3478317c0955eece332489279f2252f9bc
Artur Mihno <artur.mihno@adm.ee>
2016-09-19 14:48:53 +0300

    Add webpack middleware

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var path = require('path');
 
+// webpack
+var config = require('../webpack.config.js');
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+
 var app = express();
+
+// Use webpack in dev mode
+var compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler, {noInfo: true, publickPath: config.output.publickPath}));
+app.use(webpackHotMiddleware(compiler));
 
 app.use(express.static('./dist'));
 
